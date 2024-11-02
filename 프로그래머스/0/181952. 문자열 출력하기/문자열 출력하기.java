1678daf27e4a992659b6a01b76eb6176ab0666ea
minchae <156808313+minchae0505@users.noreply.github.com>
2024-11-02 20:12:58 +0900

    [level 0] Title: 문자열 출력하기, Time: 137.88 ms, Memory: 62.9 MB -BaekjoonHub

--- 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
@@ -1,9 +1,10 @@
-import java.util.Scanner; // 입력을 받기 위해 Scanner클래스를 사용
+import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String a = sc.next(); //입력받은 문자열 받아오기
-        System.out.printf("%s", a);
+        String a = sc.next();
+        
+        System.out.print(a);
     }
 }
